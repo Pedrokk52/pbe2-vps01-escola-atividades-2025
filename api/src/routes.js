5b7bcb908de7c1076a6564d15128e31a27c21b42
Pedrokk52 <pedrooff2021@gmail.com>
2025-05-13 15:14:03 -0300

    .

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = express.Router();
 
+const Aluno = require('./controllers/alunos')
+
 routes.get('/', (req, res) => {
   return res.json({ titulo: 'Escola ACME' });
 });
+
+routes.post('/alunos',Aluno.create);
+routes.get('/alunos',Aluno.read);
 
 module.exports = routes;
